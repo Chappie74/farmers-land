83ccd0c31c0961c7e43fcac39f75d9fddd0f262b
Chappie74 <seanmsingh7@gmail.com>
2018-04-16 00:25:22 -0400

    Created new project; Added bootstrap 4 theme.  Updates nugaet packages

---
+++ farmers-land/farmers-land/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(farmers_land.Startup))]
+namespace farmers_land
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
